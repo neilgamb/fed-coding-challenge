ff0cbeb38ff769041d697122f06f061519402992
Neilson Gamble <neilgamb@Neilsons-MacBook-Pro.local>
2017-07-13 11:39:26 -0400

    Redudant doc ready removed

--- app.js
+++ app.js
@@ -5,26 +5,24 @@ window.addEventListener('load', function () {
 
         console.log(data);
 
-        $('#thetable tr').not(':first').not(':last').remove();
+        $('#thetable tr').not(':first').remove();
         let html = '';
         for (let i = 0; i < data.length; i++) {
             html += '<tr><td>' + data[i].name + '</td><td>' + data[i].apy + "%" + '</td><td>' + "$" + data[i].earnings.toFixed(2);
         }
-        $('#thetable tr').first().after(html);
         $("th").css("background-color", "#ffffff");
+        $('#thetable tr').first().after(html);
         $("tr:even").css("background-color", "#DDEBE1");
         $("tr:odd").css("background-color", "#EEF5F0");
     });
 
     // Add / remove "active" class to navigation li's on header and footer
     // for styling purposes
-    $('document').ready(function () {
         $('#links a').click(function (e) {
             $('#links a').removeClass('active');
             $('#footer-links a').removeClass('active');
             $(this).addClass('active');
         });
-    });
 
     // sidebar navigation toggle "active" class for styling changes
     $("#newsButton").click(function () {
